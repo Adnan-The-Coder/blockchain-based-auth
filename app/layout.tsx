336eab830cd45f0d8430348ae00edd65c25d0e6e
Adnan <adnanali23189@gmail.com>
2025-05-06 12:24:47 +0530

    fix: Lint Errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -32,8 +33,8 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="dark"
           >
-            {children}
-          </ThemeProvider>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import SignUpForm from "@/components/sign-up-form";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <SignUpForm/>
-    </>
+    </div>  
   );
 }
